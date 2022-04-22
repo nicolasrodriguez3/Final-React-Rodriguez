c1a4e372e8eb9d97da5c29f5f0d395e30ef8d13a
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2022-04-22 16:31:19 -0300

    trabajando en itemCount

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from "react"
+import "./ItemCount.css"
 
 export default function ItemCount(props) {
 	const [value, setValue] = useState(1)
+
+
+	const addToCart = () => {
+		setValue(1)
+	}
 	return (
 		<>
 			<div className="item-count">
-				<button onClick={()=>{setValue(value+1)}}>+</button>
+				<button onClick={()=>{setValue(value+1)}} disabled={value >= props.stock && true} >+</button>
 				<span>{value}</span>
-				<button onClick={()=>{setValue(value-1)}}>-</button>
+				<button onClick={()=>{setValue(value-1)}} disabled={value <= 1 && true} >-</button>
 			</div>
-			<button>Agregar al carrito</button>
+			<button className="add-cart" onClick={addToCart}>Agregar al carrito</button>
 		</>
 	)
 }
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -6,9 +6,11 @@ function ItemList(props) {
 	return ( 
 		<article className='item'>
 			<img src={props.src} alt={props.title} />
-			<h3>{props.title}</h3>
-			<p>{props.price}</p>
-			<ItemCount stock={10}/>
+			<div className='item-info'>
+				<h2>{props.title}</h2>
+				<p>{props.price}</p>
+				<ItemCount stock={10}/>
+			</div>
 		</article>
 	 );
 }
