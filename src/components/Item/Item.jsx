a612fe6b064380a3d837c7084f9e2a0fa0e26071
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2022-05-02 13:38:19 -0300

    fix: change fetch to make request to local json

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -7,7 +7,7 @@ export default function Item({product}) {
 
 	return (
 		<article className="item">
-			<img src={product.thumbnail} alt={product.title} />
+			<img src={product.pictureURL} alt={product.title} />
 			<div className="item-info">
 				<h2>{product.title}</h2>
 				<p>{product.price}</p>
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -4,14 +4,24 @@ import "./ItemList.css"
 
 function ItemList(props) {
 	const [products, setProducts] = useState([])
-	const query = "samsung" //* provisorio
+	// const query = "samsung" //* provisorio
 
+	// useEffect(() => {
+	// 	fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
+	// 		.then((res) => res.json())
+	// 		.then((data) => {
+	// 			setProducts(data.results)
+	// 		})
+	// }, [])
 	useEffect(() => {
-		fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${query}`)
-			.then((res) => res.json())
-			.then((data) => {
-				setProducts(data.results)
-			})
+		fetch("./JSON/productos.json")
+		.then(res => res.ok ? res.json(res) : Promise.reject(res))
+    .then(data => {
+      console.log(data);
+			setProducts(data)
+    })
+    .catch(err => console.log(err));
+
 	}, [])
 
 	return products.length === 0 ? (
