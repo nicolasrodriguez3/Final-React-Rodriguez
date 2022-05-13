aba9ac4960cc2cce322bea905bcb2e707fb40464
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2022-05-13 16:23:05 -0300

    connected to firebase

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -4,23 +4,25 @@ import { DotPulse } from '@uiball/loaders'
 import Item from "../Item/Item"
 import "./ItemList.css"
 
-function ItemList(props) {
-	const [products, setProducts] = useState([])
+function ItemList({products}) {
+	//const [products, setProducts] = useState([])
 
 	useEffect(() => {
-		fetch("/JSON/productos.json")
-		.then(res => res.ok ? res.json(res) : Promise.reject(res))
-    .then(data => {
-			if(props.category){
-				setProducts(data.filter(product => product.category === props.category))
-			}
-			else{
-				setProducts(data)
-			}
-    })
-    .catch(err => console.log(err))
+		// fetch("/JSON/productos.json")
+		// .then(res => res.ok ? res.json(res) : Promise.reject(res))
+    // .then(data => {
+		// 	if(props.category){
+		// 		setProducts(data.filter(product => product.category === props.category))
+		// 	}
+		// 	else{
+		// 		setProducts(data)
+		// 	}
+    // })
+    // .catch(err => console.log(err))
 
-	}, [props.category])
+
+
+	}, [products])
 
 	return products.length === 0 ? (
 		<DotPulse size={40} speed={1.3} color="#7c2cab" />
--- src/pages/ItemListContainer/ItemListContainer.jsx
+++ src/pages/ItemListContainer/ItemListContainer.jsx
@@ -1,18 +1,29 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-
+import db from "../../firebase/firebaseConfig"
+import { collection, getDocs } from "firebase/firestore"
 import ItemList from "../../components/ItemList/ItemList"
 import "./ItemListContainer.css"
 
 export default function ItemListContainer(props) {
-	const {category} = useParams()
+	const { category } = useParams()
+	const [products, setProducts] = useState([])
 
+	useEffect(() => {
+		const getData = async () => {
+			const data = await getDocs(collection(db, "items"))
+			const products = data.docs.map((product) => {
+				return { id: product.id, ...product.data() }
+			})
+			setProducts(products)
+		}
+		getData()
+	}, [])
 	return (
 		<div className="item-list-container">
-			
 			<h2>{props.greeting}❤️</h2>
 			<section className="max-width item-list">
-				<ItemList category={category}/>
+				<ItemList products={products} />
 			</section>
 		</div>
 	)
