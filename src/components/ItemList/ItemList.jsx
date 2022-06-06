0904fd14ad2acefd7a8fc3f25d8c14ebc4345c86
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2022-06-05 23:20:50 -0300

    infinite scroll with array.slice

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react"
-import { collection, getDocs, limit, query, where } from "firebase/firestore"
+import { collection, getDocs, query, where } from "firebase/firestore"
 import db from "../../firebase/firebaseConfig"
-import { Link, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 import InfiniteScroll from "react-infinite-scroll-component"
 import Item from "../Item/Item"
 import ItemSkeleton from "../ItemSkeleton/ItemSkeleton"
@@ -13,6 +13,7 @@ const productsRef = collection(db, "items")
 export default function ItemList() {
 	const { category } = useParams()
 	const [products, setProducts] = useState([])
+	const [productsShowing, setProductsShowing] = useState([])
 	const [loading, setLoading] = useState(true)
 	const [hasMore, setHasMore] = useState(true)
 
@@ -20,10 +21,20 @@ export default function ItemList() {
 	const LIMIT = 12 * page
 
 	// Verificar si hay mas productos para cargar
-	console.log(products.length , LIMIT)
 	useEffect(() => {
-	(products.length < LIMIT) && setHasMore(false)
+		products.length < LIMIT ? setHasMore(false) : setHasMore(true)
+		console.log("has more", products.length, LIMIT, products.length < LIMIT)
 	}, [products, LIMIT])
+
+	// const hasMoreProducts = () => {
+	// 	console.log(products.length > LIMIT)
+	// 	return products.length > LIMIT
+	// }
+
+	// Recortar el array de productos segun el numero de pagina
+	const sliceProducts = (products, limit) => {
+		return products.slice(0, limit)
+	}
 
 	// Resetear el estado "page" cuando se cambie la categoria y hacer scroll hacia arriba
 	useEffect(() => {
@@ -32,25 +43,28 @@ export default function ItemList() {
 		window.scrollTo(0, 0)
 	}, [category])
 
+	// obtener los datos de firestore
 	useEffect(() => {
-		// obtener los datos de firestore
 		const getData = async (category) => {
-			const q = category
-				? query(productsRef, where("category", "==", category), limit(LIMIT))
-				: query(productsRef, limit(LIMIT))
+			const q = category ? query(productsRef, where("category", "==", category)) : query(productsRef)
 
 			const data = await getDocs(q)
 			const products = data.docs.map((product) => {
 				return { id: product.id, ...product.data() }
 			})
 			setProducts(products)
+			setProductsShowing(sliceProducts(products, LIMIT))
 			setLoading(false)
 		}
 		getData(category)
 	}, [category, LIMIT])
 
-	const loadMore = async () => {
+	const loadMore = () => {
+		console.log(page)
 		setPage((prevPage) => prevPage + 1)
+		const productsToShow = sliceProducts(products, LIMIT)
+		setProductsShowing(productsToShow)
+		console.log(productsToShow, page)
 	}
 
 	const LoadingUI = () => {
@@ -68,18 +82,24 @@ export default function ItemList() {
 		LoadingUI()
 	) : (
 		<InfiniteScroll
-			dataLength={products.length} //This is important field to render the next data
+			dataLength={250}
 			next={loadMore}
 			hasMore={hasMore}
 			loader={<LoadingUI />}
 			endMessage={
 				<div style={{ textAlign: "center" }}>
-					<p>No hay mas productos!</p> 
-					<p><button onClick={()=> window.scrollTo(0,0)} className="btn">Ir arriba</button></p>
+					<p>No hay mas productos!</p>
+					<p>
+						<button onClick={() => window.scrollTo(0, 0)} className="btn">
+							Ir arriba
+						</button>
+					</p>
 				</div>
 			}>
 			<section className="item-list">
-				{products.map((product) => <Item key={product.id} product={product} />)}
+				{productsShowing.map((product) => (
+					<Item key={product.id} product={product} />
+				))}
 			</section>
 		</InfiniteScroll>
 	)
